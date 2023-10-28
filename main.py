65efe6c8eb18f996b7e85ccead38e514284076cb
Pankratov Maxim <linuxhallo@gmail.com>
2023-10-28 12:48:58 +0300

    Added design

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import socket
 ###########################
 green = colorama.Fore.GREEN
 red = colorama.Fore.RED
+violet = '\033[95m'
 reset_color = colorama.Style.RESET_ALL
 ###########################
 
@@ -21,11 +22,24 @@ def jitter(min_secs: float, max_secs: float) -> float:
     
     return round(((min_secs + max_secs) / 2), 2)
 
+def bordered(text):
+    lines = text.splitlines()
+    width = max(len(s) for s in lines)
+    res = [f'{reset_color}{feol}┌' + '─' * width + f'{feol}{reset_color}┐']
+    count = 0
+    for s in lines:
+        a = ""
+        count += 1
+        if count == 2: a = "\t\t"
+        res.append(f'{reset_color}{feol}│{reset_color}' + (s + ' ' * width)[:width] + f'{reset_color}{feol}{a}│{reset_color}')
+    res.append(f'{reset_color}{feol}└' + f'{feol}─' * width + f'{feol}┘{reset_color}')
+    return '\n'.join(res)
+
 def main() -> NoReturn:
     if len(sys.argv) > 1: 
         IPs = [socket.gethostbyname(ip) for ip in sys.argv[1:]] #hosts to ping
     else: 
-        print("No hosts to ping!")
+        print(f"{reset_color}No hosts to ping!{reset_color}")
         return None
 
     for host in IPs:
@@ -36,11 +50,11 @@ def main() -> NoReturn:
         else:
             status = f"{reset_color}{green}OK{reset_color}"
 
-        print(f"IP: {host} \n \
+        print(bordered(f"IP: {host} \n \
             Status: {status} \n \
             Speed: {ping_response.rtt_min_ms} ms \n \
             Packets Lost: {ping_response.stats_packets_lost}/3 packets \n \
-            Jitter: {jitter(ping_response.rtt_min_ms, ping_response.rtt_max_ms)} ms")
+            Jitter: {jitter(ping_response.rtt_min_ms, ping_response.rtt_max_ms)} ms"))
 
 if __name__ == "__main__":
     main()

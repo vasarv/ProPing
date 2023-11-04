d122cd3f7cb4ed6b6182c172a8d5c9537d4be950
VasarV <76836836+vasarv@users.noreply.github.com>
2023-11-04 16:06:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from pythonping import ping as ping_func
 import sys
 import socket
 
-###########################
+##### Color samples in variables #####
 green = colorama.Fore.GREEN
 red = colorama.Fore.RED
 violet = '\033[95m'
 reset_color = colorama.Style.RESET_ALL
-###########################
+######################################
 
 def ping(host: str) -> list:
     """Ping host function"""
-    response = ping_func(host, count=3)
 
-    return response
+    return ping_func(host, count=3) # Ping the IP and return the result
 
 def jitter(min_secs: float, max_secs: float) -> float:
     """Add jitter to a sleep call"""
     
-    return round(((min_secs + max_secs) / 2), 2)
+    return round(((min_secs + max_secs) / 2), 2) # Calculate the arithmetic mean between the largest and smallest values, round to hundredths and return the result
 
 def bordered(text: str, add: int = 0, rm: int = 0) -> str:
+    """A function that creates a frame for text"""
     lines = text.splitlines()
     width = max(len(s) for s in lines)
     res = [f'{reset_color}{violet}┌' + '─' * width + f'{violet}┐{reset_color}']
@@ -37,6 +37,7 @@ def bordered(text: str, add: int = 0, rm: int = 0) -> str:
     return '\n'.join(res)
 
 def main() -> NoReturn:
+    """Main function"""
     if len(sys.argv) > 1: 
         IPs = [socket.gethostbyname(ip) for ip in sys.argv[1:]] #hosts to ping
     else: 
@@ -44,9 +45,9 @@ def main() -> NoReturn:
         return None
 
     for host in IPs:
-        ping_response = ping(host)
-
-        if ping_response.stats_packets_returned == 0:
+        ping_response = ping(host) # Ping the ip and save it to a variable
+        
+        if ping_response.stats_packets_returned == 0: # Checking if the ip is available
             status = f"{reset_color}{red}Offline{reset_color}"
         else:
             status = f"{reset_color}{green}OK{reset_color}"
@@ -55,7 +56,7 @@ def main() -> NoReturn:
             Status: {status} \n \
             Speed: {ping_response.rtt_min_ms} ms \n \
             Packets Lost: {ping_response.stats_packets_lost}/3 packets \n \
-            Jitter: {jitter(ping_response.rtt_min_ms, ping_response.rtt_max_ms)} ms", add=13))
+            Jitter: {jitter(ping_response.rtt_min_ms, ping_response.rtt_max_ms)} ms", add=13)) # Оutput the ping result
 
 if __name__ == "__main__":
     main()
